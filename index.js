9f32e85605960c099e54db21503e60659a6a615d
DDulionaire <tigk0070@gmail.com>
2019-06-16 21:01:03 -0700

    node-fetch

---
+++ index.js
@@ -0,0 +1,11 @@
+//const graphqlYoga = require("graphql-yoga")
+import {GraphQLServer} from "graphql-yoga";
+import resolvers from "./graphql/resolvers"
+
+
+const server = new GraphQLServer({
+    typeDefs: "./graphql/schema.graphql",
+    resolvers
+});
+
+server.start(() => console.log("graphQL Server running"));
