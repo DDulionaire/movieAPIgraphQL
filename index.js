7e5d35fb6cc24745f5bea9727c416f82528440aa
DDulionaire <tigk0070@gmail.com>
2019-06-18 04:56:35 -0700

    suggestion

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-//const graphqlYoga = require("graphql-yoga")
-import {GraphQLServer} from "graphql-yoga";
-import resolvers from "./graphql/resolvers"
-
+import { GraphQLServer } from "graphql-yoga";
+import resolvers from "./graphql/resolvers";
 
 const server = new GraphQLServer({
-    typeDefs: __dirname + "/graphql/schema.graphql",
-    resolvers,
-    playground: true,
-    introspection: true
+  typeDefs: `type Movie {
+    id: Int!
+    title: String!
+    rating: Float
+    description_intro: String
+    language: String
+    medium_cover_image: String
+    genres: [String]
+  }
+  
+  type Query {
+    movies(limit: Int, rating: Float): [Movie]!
+    movie(id: Int!): Movie
+    suggestions(id: Int!): [Movie]!
+  }
+  `,
+  resolvers
 });
 
-server.start(() => console.log("graphQL Server running"));
+server.start(() => console.log("Graphql Server Running"));
